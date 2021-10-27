8154437fad245d5d2b14db508fa3435c6e6996b1
Java Overone <java.overone@gmail.com>
2021-10-27 21:24:37 +0300

    Hi there commit

--- src/by/overone/test/Main.java
+++ src/by/overone/test/Main.java
@@ -3,5 +3,6 @@ package by.overone.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hi there!");
     }
 }
